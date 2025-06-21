85871bb51ce6020ec32e96c3defdb3e5f8185b6d
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-06-21 22:50:58 +1200

    simplified

--- algorithms/shell.py
+++ algorithms/shell.py
@@ -1,113 +1,17 @@
-import math
 from misc import finished_animation
 
 
-def shell_gaps(n):
-    gaps = []
-    curr = n // 2
-    while curr > 0:
-        gaps.append(curr)
-        curr //= 2
-    return gaps
-
-
-def hibbard_gaps(n):
-    gaps = []
-    i = 1
-    while (gap := 2 ** i - 1) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def papernov_stasevich_gaps(n):
-    gaps = [1]
-    i = 1
-    while (gap := 2 ** i + 1) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def knuth_gaps(n):
-    gaps = []
-    i = 1
-    while (gap := (3 ** i - 1) // 2) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def sedgewick_1982_gaps(n):
-    gaps = [1]
-    i = 1
-    while (gap := 4 ** i + 3 * 2 ** (i - 1) + 1) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def sedgewick_1986_gaps(n):
-    gaps = [1]
-    i = 1
-    while (gap := 9 * (2 ** i - 2 ** (i / 2)) + 1 if i % 2 == 0 else 8 * 2 ** i - 6 * 2 ** ((i + 1) / 2) + 1) < n:
-        gaps.append(int(gap))
-        i += 1
-    return gaps[::-1]
-
-
-def tokuda_gaps(n):
-    gaps = []
-    i = 1
-    while (gap := math.ceil((1 / 5) * (9 * (9 / 4) ** (i - 1) - 4))) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def ciura_gaps(n):
-    return [701, 301, 132, 57, 23, 10, 4, 1]
-
-
-def lee_gaps(n):
-    GAMMA = 2.243609061420001
-    gaps = []
-    i = 1
-    while (gap := math.ceil((GAMMA ** i - 1) / (GAMMA - 1))) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def skean_ehrenborg_jaromczyk_gaps(n):
-    gaps = [1, 4]
-    i = 1
-    while (gap := math.floor(4.0816 * 8.5714 ** (i / 2.2449))) < n:
-        gaps.append(gap)
-        i += 1
-    return gaps[::-1]
-
-
-def shell_sort(arr, sequence="shell"):
-    SEQUENCES = {
-        "shell": shell_gaps,
-        "hibbard": hibbard_gaps,
-        "papernov": papernov_stasevich_gaps,
-        "knuth": knuth_gaps,
-        "sedgewick82": sedgewick_1982_gaps,
-        "sedgewick86": sedgewick_1986_gaps,
-        "tokuda": tokuda_gaps,
-        "ciura": ciura_gaps,
-        "lee": lee_gaps,
-        "skean": skean_ehrenborg_jaromczyk_gaps
-    }
-
-    if sequence not in SEQUENCES:
-        raise ValueError(
-            f"Invalid partition scheme '{sequence}'. Supported schemes are: {', '.join(SEQUENCES.keys())}.")
+def shell_sort(arr):
+    def shell_gaps(n):
+        gaps = []
+        curr = n // 2
+        while curr > 0:
+            gaps.append(curr)
+            curr //= 2
+        return gaps
 
     n = len(arr)
-    gaps = SEQUENCES[sequence](n)
+    gaps = shell_gaps(n)
 
     for gap in gaps:
         for i in range(gap, n):
